6f8646866ada4c9e0cb7a9f6267eda769c2de10b
Ana Cristina PG <anacris.pg91@gmail.com>
2016-07-20 20:04:50 +0200

    shopping_Cart

---
+++ Ruby/Exercise-ShoppingCart/shopping_Cart.rb
@@ -0,0 +1,98 @@
+
+###Shopping cart
+
+class ShoppingCart
+  def initialize
+    @items = []
+  end
+
+  def add_item(item)
+  	@items.push(item)
+  end
+
+  def checkout
+  	total = 0
+  	@items.each do |item|
+  		total += item.price
+  	end 
+
+  	if @items.size >= 5
+  		total = total * 0.9
+  	end
+  	
+  	total
+  end
+
+  
+
+end
+
+
+class Item 
+  attr_accessor :price
+  def initialize(name, price)
+      @name = name
+      @price = price
+  end
+
+  def price
+      return @price
+  end
+end
+
+class Houseware < Item
+  	def price
+    	if @price > 100 
+    		@price = @price * 0.95
+    	end
+    	@price
+  	end
+end
+
+class Fruit < Item
+	def price	    
+	    time = Time.new
+	    day = time.to_a[6]
+
+	  	if day == 0 or day == 6
+	  		@price = @price * 0.90
+		end
+		return @price
+	end	
+end
+
+
+joshs_cart = ShoppingCart.new
+
+banana = Fruit.new("Banana", 10)
+oj = Item.new("Orange Juice", 10)
+rice = Item.new("Rice", 1)
+anchovies = Item.new("Anchovies", 2)
+vacuum_cleaner = Houseware.new("Vacuum Cleaner", 150)
+
+
+joshs_cart.add_item(oj)
+joshs_cart.add_item(rice)
+joshs_cart.add_item(banana)
+joshs_cart.add_item(vacuum_cleaner)
+joshs_cart.add_item(anchovies)
+puts "Your total is #{joshs_cart.checkout}€. Have a nice day!"
+
+
+=begin	
+puts "Banana = #{banana.price}€"
+puts "Orange juice = #{orange_juice.price}€"
+puts "Rice = #{rice.price}€"
+puts "Anchovies = #{anchovies.price}€"
+puts "Vacuum Cleaner = #{vacuum_cleaner.price}€"
+=end
+
+
+
+
+
+
+
+
+
+
